ef28b9a6684f27774aa0d78a75c6eb538e29c3c0
Nicolaspz <nicolaspz14@hotmail.com>
2023-02-05 12:55:37 +0100

    configurando as rolles  e permiçóes de acesso

--- resources/views/includes/pane/menu.blade.php
+++ resources/views/includes/pane/menu.blade.php
@@ -1,32 +1,54 @@
 <ul class="navbar-nav">
+    @can('user')
+    <li class="nav-item">
+        <a class="nav-link " href="./examples/icons.html">
+          <i class="ni ni-planet text-blue"></i> Vagas
+        </a>
+      </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('logout')}}"
+          onclick="event.preventDefault(); document.getElementById('formLogout').submit();"
+        >
+          <i class="fas fa-sign-in-alt"></i> Sair
+        </a>
+        <form action="{{route('logout')}}" method="POST" style="display: none;" id="formLogout">
+          @csrf
+      </form>
+      </li>
+
+    @elsecan('admin')
     <li class="nav-item  active ">
-      <a class="nav-link  active " href="./index.html">
-        <i class="ni ni-tv-2 text-primary"></i> Relatórios
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link " href="./examples/icons.html">
-        <i class="ni ni-planet text-blue"></i> Vagas
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link " href="./examples/maps.html">
-        <i class="far fa-newspaper text-orange"></i> Candidatos
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link " href="./examples/profile.html">
-        <i class="ni ni-single-02 text-yellow"></i>Perfil de Usuário
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{route('logout')}}"
-        onclick="event.preventDefault(); document.getElementById('formLogout').submit();"
-      >
-        <i class="fas fa-sign-in-alt"></i> Sair
-      </a>
-      <form action="{{route('logout')}}" method="POST" style="display: none;" id="formLogout">
-        @csrf
-    </form>
-    </li>
+        <a class="nav-link  active " href="./index.html">
+          <i class="ni ni-tv-2 text-primary"></i> Relatórios
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./examples/icons.html">
+          <i class="ni ni-planet text-blue"></i> Vagas
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./examples/maps.html">
+          <i class="far fa-newspaper text-orange"></i> Candidatos
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./examples/profile.html">
+          <i class="ni ni-single-02 text-yellow"></i>Perfil de Usuário
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('logout')}}"
+          onclick="event.preventDefault(); document.getElementById('formLogout').submit();"
+        >
+          <i class="fas fa-sign-in-alt"></i> Sair
+        </a>
+        <form action="{{route('logout')}}" method="POST" style="display: none;" id="formLogout">
+          @csrf
+      </form>
+      </li>
+    @endcan
+
+
+
   </ul>
